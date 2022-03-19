7f5e3f1369c2d4bbfd9e1fd57eca2f07e675eef3
ZhangGongjie <gongjiezhang@ntu.edu.sg>
2022-03-19 15:53:38 +0800

    update

--- models/fast_detr.py
+++ models/fast_detr.py
@@ -147,7 +147,8 @@ class FastDETR(nn.Module):
         # this is a workaround to make torchscript happy, as torchscript
         # doesn't support dictionary with non-homogeneous values, such
         # as a dict having both a Tensor and a list.
-        return [{'pred_logits': a, 'pred_boxes': b} for a, b in zip(outputs_class[:-1], outputs_coords[:-1])]
+        return [{'pred_logits': a, 'pred_boxes': b}
+                for a, b in zip(outputs_class[:-1], outputs_coords[:-1])]
 
 
 class SetCriterion(nn.Module):
