16d79d325df62fe6ec5ee6c1473593eb02a8c563
ZhangGongjie <gongjiezhang@ntu.edu.sg>
2022-04-15 23:02:00 +0800

    fix bug for dataset dir. (issue #3)

--- datasets/coco.py
+++ datasets/coco.py
@@ -151,9 +151,9 @@ def build(image_set, args):
     assert root.exists(), f'provided COCO path {root} does not exist'
     mode = 'instances'
     PATHS = {
-        "train": (root / "images" / "train2017", root / "annotations" / f'{mode}_train2017.json'),
-        "val": (root / "images" / "val2017", root / "annotations" / f'{mode}_val2017.json'),
-        "test": (root / "images" / "test2017", root / "annotations" / f'image_info_test-dev2017.json'),
+        "train": (root / "train2017", root / "annotations" / f'{mode}_train2017.json'),
+        "val": (root / "val2017", root / "annotations" / f'{mode}_val2017.json'),
+        "test": (root / "test2017", root / "annotations" / f'image_info_test-dev2017.json'),
     }
 
     img_folder, ann_file = PATHS[image_set]
